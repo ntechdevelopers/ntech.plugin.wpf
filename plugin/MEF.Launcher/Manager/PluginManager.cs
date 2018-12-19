828e462ae4576c14ae0e0d97ebd67eba33aa1acd
namvut <vuthanhnam94@gmail.com>
2018-12-19 15:14:51 +0700

    Add project plugin wpf

---
+++ plugin/MEF.Launcher/Manager/PluginManager.cs
@@ -0,0 +1,39 @@
+﻿using MEF.Launcher.Contract;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+
+namespace MEF.Launcher.Manager
+{
+    [Export(typeof(IPluginManager))]
+    [PartCreationPolicy(CreationPolicy.Shared)]
+    public class PluginManager : IPluginManager
+    {
+        /// <summary>
+        /// Gets or sets plugin.
+        /// </summary>
+        [ImportMany(AllowRecomposition = true)]
+        public IEnumerable<IPlugin> Plugins { get; set; }
+
+        /// <summary>
+        /// Loads the plugin.
+        /// </summary>
+        public void LoadPlugin()
+        {
+            foreach (var plugin in this.Plugins)
+            {
+                plugin.InitPlugin();
+            }
+        }
+
+        /// <summary>
+        /// Unloads the plugin.
+        /// </summary>
+        public void UnloadPlugin()
+        {
+            foreach (var plugin in this.Plugins)
+            {
+                plugin.UninitPlugin();
+            }
+        }
+    }
+}
---
+++ plugin/Plugins/BasicPlugin/BasicPlugin.cs
@@ -0,0 +1,58 @@
+﻿using BasicPlugin.ViewModels;
+using Caliburn.Micro;
+using MEF.Launcher.Contract;
+using MEF.Launcher.Contract.IoC;
+using MEF.Launcher.Platform.Plugin;
+using MEF.Launcher.Platform.ViewModels;
+using System.ComponentModel.Composition;
+
+namespace BasicPlugin
+{
+    [Export(typeof(IPlugin))]
+    [PartCreationPolicy(CreationPolicy.Shared)]
+    public class BasicPlugin : PluginBase<BasicPlugin>
+    {
+        /// <summary>
+        /// Gets or sets the name of the pluign.
+        /// </summary>
+        /// <value>
+        /// The name of the pluign.
+        /// </value>
+        public override string PluignName { get; set; } = "Basic plugin";
+
+        /// <summary>
+        /// Gets or sets the plugin version.
+        /// </summary>
+        /// <value>
+        /// The plugin version.
+        /// </value>
+        public override string PluginVersion { get; set; } = "1.0.0.0";
+
+        /// <summary>
+        /// Initializes the plugin.
+        /// </summary>
+        public override void InitPlugin()
+        {
+            var mainViewModel = SimpleIoC.Get<LeftdownMenuViewModel>();
+            this.AppManager.ShowMainUI(mainViewModel);
+
+            // Register menu
+            mainViewModel.RegisterMenu(new MEF.Launcher.Platform.Menu.MenuItemEx
+            {
+                Name = "Basic Menu",
+                ClickAction = () =>
+                {
+                    mainViewModel.ActivateItem(new ExampleViewModel());
+                }
+            });
+        }
+
+        /// <summary>
+        /// Uninits the plugin.
+        /// </summary>
+        public override void UninitPlugin()
+        {
+
+        }
+    }
+}
